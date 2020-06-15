171b75d249060efca3cb8ff580bc5a0536a70688
dtrongphuc <dtrongphucdv@gmail.com>
2020-06-15 22:47:22 +0700

    first commit

---
+++ Reciple/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Reciple
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click()
+        {
+        }
+    }
+}
