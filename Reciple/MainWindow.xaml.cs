6d32147182273d95914f964e9b1c9c0b04491570
dtrongphuc <dtrongphucdv@gmail.com>
2020-06-17 17:33:23 +0700

    change slide layout & fix favorite icon & search textbox

--- Reciple/MainWindow.xaml.cs
+++ Reciple/MainWindow.xaml.cs
@@ -35,7 +35,7 @@ namespace Reciple
             ShowHideMenu("sbHideLeftMenu", btnMenuHide, btnMenuShow, Menu);
         }
 
-        private void ShowHideMenu(string Storyboard, Button btnHide, Button btnShow, StackPanel pnl)
+        private void ShowHideMenu(string Storyboard, Button btnHide, Button btnShow, DockPanel pnl)
         {
             Storyboard sb = Resources[Storyboard] as Storyboard;
             sb.Begin(pnl);
